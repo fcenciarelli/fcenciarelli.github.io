4907e55313f987eb76ac79bb2e0263aa3c991e5f
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2022-07-19 23:35:26 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-new hoverEffect({
-    parent: document.querySelector(".distortion"),
-    intensity: 0.2,
-    image1: "./mac.png",
-    image2: './pen.png',
-    displacementImage: "./assets/stripe1.png"
-});
